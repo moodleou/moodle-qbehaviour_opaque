d2ff16b449329e03cef99447fb741419ecf44c55
Tim Hunt <T.J.Hunt@open.ac.uk>
2011-10-20 17:55:17 +0100

    Add a version.php file.

---
+++ version.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information for the Opaque question behaviour.
+ *
+ * @package    qbehaviour
+ * @subpackage opaque
+ * @copyright  2006 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component    = 'qbehaviour_opaque';
+$plugin->version      = 2011092601;
+$plugin->maturity     = MATURITY_BETA;
+
+$plugin->requires     = 2011051212;
+$plugin->dependencies = array(
+    'qtype_opaque'    => 2011092600
+);
